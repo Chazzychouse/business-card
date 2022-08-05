118f2f568b0df9950e41d444dc100903aecc43b8
Chazzychouse <acecartwright101@hotmail.com>
2022-08-05 14:46:26 -0400

    "added hyperlinks and email functionality"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,18 @@ import youtube from "../images/youtube.png";
 export default function Footer() {
   return (
     <div id="logo-wrapper">
-      <a href="https://www.facebook.com/chase.cartwright.3">
+      <a
+        href="https://www.facebook.com/chase.cartwright.3"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={facebook} alt="facebook" id="facebook" />
       </a>
-      <a href="https://www.youtube.com/channel/UCZqUuli_95AQZcdNHABbSIw">
+      <a
+        href="https://www.youtube.com/channel/UCZqUuli_95AQZcdNHABbSIw"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={youtube} alt="youtube" id="youtube" />
       </a>
     </div>
--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,39 @@
-import headshot from "../images/headshot.png"
+import headshot from "../images/headshot.png";
 
 export default function Info() {
-    return (
-        <div id="headshot-wrapper">
-            <img src={headshot} alt="a" id="headshot" />
-            <h1 id="name-header">Chase Cartwright</h1>
-            <p id="job-title">Full Stack Developer</p>
-            <small id="website">www.chasecartwright.com</small>
-            <p><button className="button" id="email">Email</button><button className="button" id="linkedin">LinkedIn</button></p>
-        </div>
-    );
+  return (
+    <div id="headshot-wrapper">
+      <img src={headshot} alt="a" id="headshot" />
+      <h1 id="name-header">Chase Cartwright</h1>
+      <p id="job-title">Full Stack Developer</p>
+      <a
+        href="https://github.com/Chazzychouse"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <small id="website">www.github.com/chazzychouse</small>
+      </a>
+
+      <p>
+        <a
+          href="mailto:chasecartwright757@gmail.com?subject=subject text"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button" id="email">
+            Email
+          </button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/chase-cartwright-98963823a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button" id="linkedin">
+            LinkedIn
+          </button>
+        </a>
+      </p>
+    </div>
+  );
 }
